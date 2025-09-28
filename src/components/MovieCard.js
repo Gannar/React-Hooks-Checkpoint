cf7a1f5b6c38770f17230a64fc35c7ea82e9c30f
MeriamGANNAR <meriam.gannar@simav.tn>
2025-09-28 14:17:11 +0100

    app

---
+++ src/components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+function MovieCard({ movie }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={movie.posterURL} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+        <Card.Text>⭐ {movie.rating}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MovieCard;
